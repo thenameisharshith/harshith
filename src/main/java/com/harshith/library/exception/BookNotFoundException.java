7f57e3b6dbb7eaccfeba89e57d3cdf38fd39305c
M SAI HARSHITH GOUD <166594237+thenameisharshith@users.noreply.github.com>
2026-09-18 10:32:44 +0530

    Add book not found exception

---
+++ src/main/java/com/harshith/library/exception/BookNotFoundException.java
@@ -0,0 +1,8 @@
+package com.harshith.library.exception;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(Long id) {
+        super("Book not found with id: " + id);
+    }
+}
